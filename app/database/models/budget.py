5bd451882abea338ed0b5516a3af3324c6bb5815
Toskyuu <robert.laski10@gmail.com>
2024-11-08 15:34:18 +0100

    add budget model

---
+++ app/database/models/budget.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, Float, ForeignKey, Date
+from sqlalchemy.orm import relationship
+from app.database.postgres_utils import Base
+
+
+class Budget(Base):
+    __tablename__ = "budgets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+    limit = Column(Float, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    month_year = Column(Date, nullable=False)
+
+
+    category = relationship("Category", back_populates="budgets")
+    user = relationship("User", back_populates="budgets")
