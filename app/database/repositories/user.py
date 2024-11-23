0e20bb5a3f26bf4494ce08096758e6e4bb3369fd
Toskyuu <robert.laski10@gmail.com>
2024-11-23 17:34:23 +0100

    Refaktoring, dodanie relacji kategorii z userem, zmiana repozytory usera na klase

--- app/database/repositories/user.py
+++ app/database/repositories/user.py
@@ -4,23 +4,26 @@ from app.services.auth import get_password_hash
 from sqlalchemy.exc import IntegrityError
 
 
-def create_user(db: Session, username: str, email: str, password: str):
-    existing_user = db.query(User).filter((User.email == email)).first()
-    if existing_user:
-        raise ValueError("Użytkownik o podanym email już istnieje.")
+class UserRepository:
+    @staticmethod
+    def create_user(db: Session, username: str, email: str, password: str):
+        existing_user = db.query(User).filter((User.email == email)).first()
+        if existing_user:
+            raise ValueError("Użytkownik o podanym email już istnieje.")
 
-    hashed_password = get_password_hash(password)
-    db_user = User(username=username, email=email, password=hashed_password)
-    db.add(db_user)
+        hashed_password = get_password_hash(password)
+        db_user = User(username=username, email=email, password=hashed_password)
+        db.add(db_user)
 
-    try:
-        db.commit()
-    except IntegrityError:
-        db.rollback()
-        raise ValueError("Wystąpił problem z zapisem użytkownika.")
+        try:
+            db.commit()
+        except IntegrityError:
+            db.rollback()
+            raise ValueError("Wystąpił problem z zapisem użytkownika.")
 
-    db.refresh(db_user)
-    return db_user
+        db.refresh(db_user)
+        return db_user
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+    @staticmethod
+    def get_user_by_email(db: Session, email: str):
+        return db.query(User).filter(User.email == email).first()
