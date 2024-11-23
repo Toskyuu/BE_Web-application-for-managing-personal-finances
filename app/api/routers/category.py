4b4f58432f3f37baf951627876db0612f4610664
Toskyuu <robert.laski10@gmail.com>
2024-11-23 18:03:53 +0100

    Dodanie logiki związanej z kategoriami

---
+++ app/api/routers/category.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database.postgres_utils import get_db
+from app.api.schemas.Category import CategoryUpdate, CategoryCreate, Category
+from app.database.repositories.category import CategoryRepository
+
+router = APIRouter()
+
+
+@router.get("/", response_model=list[Category])
+def list_categories(user_id: int, db: Session = Depends(get_db)):
+    return CategoryRepository.get_categories_by_user(db, user_id=user_id)
+
+@router.get("/{category_id}", response_model=Category)
+def get_category(category_id: int, db: Session = Depends(get_db)):
+    category = CategoryRepository.get_category(db, category_id=category_id)
+    if not category:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return category
+
+
+@router.post("/", response_model=Category)
+def create_category(category: CategoryCreate, user_id: int, db: Session = Depends(get_db)):
+    return CategoryRepository.create_category(db, category=category, user_id=user_id)
+
+
+@router.post("/categories/{category_id}")
+async def update_category(category_id: int, category_update: CategoryUpdate, db: Session = Depends(get_db)):
+    updated_category = CategoryRepository.update_category(db, category_id, category_update)
+    if not updated_category:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return updated_category
+
+
+
+
+@router.delete("/categories/{category_id}")
+async def delete_category(category_id: int, db: Session = Depends(get_db)):
+    success = CategoryRepository.delete_category(db, category_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return {"message": "Category deleted successfully"}
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from app.api.routers import user, account
+
+from app.api.routers import user, account, category
 
 app = FastAPI()
 
 app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(account.router, prefix="/account", tags=["account"])
+app.include_router(category.router, prefix="/category", tags=["category"])
+
