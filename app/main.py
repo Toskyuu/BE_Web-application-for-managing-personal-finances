5f11b474d9b76aa4e57a91d0fba4d5cf321c1476
Toskyuu <robert.laski10@gmail.com>
2024-11-30 12:19:09 +0100

    stworzenie budżetów

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.api.routers import user, account, category, transaction
+from app.api.routers import user, account, category, transaction, budget
 
 app = FastAPI()
 
@@ -8,4 +8,6 @@ app.include_router(user.user_router)
 app.include_router(account.account_router)
 app.include_router(category.category_router)
 app.include_router(transaction.transaction_router)
+app.include_router(budget.budget_router)
+
 
