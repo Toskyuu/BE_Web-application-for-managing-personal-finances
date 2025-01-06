637fff804edb88c79008a4eca9c5580e72fc06e1
Toskyuu <robert.laski10@gmail.com>
2025-01-06 17:10:07 +0100

    szybki poprawki CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routers import user, account, category, transaction, budget, recurring_transaction
 from app.database.postgres_utils import get_db
@@ -19,6 +20,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, debug=True)
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user.user_router)
 app.include_router(account.account_router)
 app.include_router(category.category_router)
