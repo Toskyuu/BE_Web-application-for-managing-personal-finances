61f7f580c5a6032bfbe98d27232bd3480365210c
Toskyuu <robert.laski10@gmail.com>
2025-01-27 15:43:23 +0100

    Lekka poprawka routera, dodanie wydatków ostatnich siedmiu dni dla wykresu

--- app/api/schemas/Dashboard.py
+++ app/api/schemas/Dashboard.py
@@ -2,17 +2,19 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-from app.api.schemas.Account import  AccountListResponse
+from app.api.schemas.Account import AccountListResponse
 from app.api.schemas.Budget import BudgetListResponse
-from app.api.schemas.Stats import SummaryResponse
+from app.api.schemas.Stats import SummaryResponse, TransactionsOverTimeResponse
 from app.api.schemas.Transaction import TransactionListResponse
 
 
 class DashboardBase(BaseModel):
     pass
 
+
 class Dashboard(DashboardBase):
     accounts: Optional[AccountListResponse] = None
-    incomes_expenses: Optional[SummaryResponse]  = None
-    transactions: Optional[TransactionListResponse]  = None
-    budgets: Optional[BudgetListResponse]  = None
+    incomes_expenses_summary: Optional[SummaryResponse] = None
+    transactions: Optional[TransactionListResponse] = None
+    budgets: Optional[BudgetListResponse] = None
+    expenses: Optional[TransactionsOverTimeResponse] = None
--- app/database/repositories/dashboard.py
+++ app/database/repositories/dashboard.py
@@ -1,10 +1,11 @@
-from datetime import date
+from datetime import date, timedelta
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.schemas.Dashboard import Dashboard
-from app.api.schemas.Stats import BaseStatFilter
+from app.api.schemas.Stats import BaseStatFilter, TransactionsOverTimeFilter
 from app.api.schemas.TransactionFilter import TransactionFilter
+from app.database.models.enums import TransactionType
 from app.database.repositories.account import AccountRepository
 from app.database.repositories.budget import BudgetRepository
 from app.database.repositories.stats import StatsRepository
@@ -29,19 +30,26 @@ class DashboardRepository:
             sort_by="id",
             order="desc",
         )
+        transaction_over_time_filter = TransactionsOverTimeFilter(
+            type=[TransactionType.OUTCOME],
+            date_from=today - timedelta(days=7),
+            date_to=today,
+            interval="Daily"
+        )
 
         accounts_response = await AccountRepository.get_accounts_by_user(db, user_id=user_id, page=1, size=5,
                                                                         sort_by="id", order="desc")
 
-        incomes_expenses_response = await StatsRepository.summary(db, incomes_expenses_filter, user_id=user_id)
+        incomes_expenses_summary_response = await StatsRepository.summary(db, incomes_expenses_filter, user_id=user_id)
 
         transactions_response = await TransactionRepository.list_transactions(db, transaction_filter, user_id=user_id)
-        budgets_response = await BudgetRepository.get_budgets_by_user(
-            db, user_id=user_id, page=1, size=10, month_year=date.today(), sort_by="id", order="desc")
+        budgets_response = await BudgetRepository.get_budgets_by_user(db, user_id=user_id, page=1, size=10, month_year=date.today(), sort_by="id", order="desc")
+        expenses_response= await StatsRepository.summary_by_time(db, transaction_over_time_filter, user_id=user_id)
 
         return Dashboard(
             accounts=accounts_response,
-            incomes_expenses_response=incomes_expenses_response,
+            incomes_expenses_summary=incomes_expenses_summary_response,
             transactions=transactions_response,
-            budgets=budgets_response
+            budgets=budgets_response,
+            expenses=expenses_response
         )
